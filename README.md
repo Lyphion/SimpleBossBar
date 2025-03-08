de5795e2f1d873767f730cbbdfeeb6f3736a7018
Lyphion <reallyphion@gmail.com>
2025-03-08 19:49:35 +0100

    Added documentation

---
+++ README.md
@@ -0,0 +1,9 @@
+# SimpleBossBar
+
+## General Information
+
+This Boss Bar plugin enables the display of information via the Boss Bar. It supports formating of the Text via [MiniMessage](https://docs.advntr.dev/minimessage/index.html) and [PlaceholderAPI](https://github.com/PlaceholderAPI/PlaceholderAPI).
+
+## Configuration
+
+The Boss Bar can be configured using the `config.yml`. There are options for the update period, text and styling options of the Boss Bar.
--- src/main/java/dev/lyphium/bossbar/BossBarManager.java
+++ src/main/java/dev/lyphium/bossbar/BossBarManager.java
@@ -34,17 +34,25 @@ public final class BossBarManager {
         Bukkit.getScheduler().runTaskTimer(plugin, this::updateBossBar, period, period);
     }
 
+    /**
+     * Load config values.
+     */
     public void loadConfig() {
         plugin.saveDefaultConfig();
         final FileConfiguration config = plugin.getConfig();
 
-        period = config.getInt("Period");
-        text = config.getString("Text");
-        progress = (float) config.getDouble("Progress");
-        color = BossBar.Color.NAMES.valueOrThrow(config.getString("Color", "red"));
-        overlay = BossBar.Overlay.NAMES.valueOrThrow(config.getString("Overlay", "progress"));
+        period = config.getInt("Period", 20);
+        text = config.getString("Text", "");
+        progress = (float) config.getDouble("Progress", 1.0);
+        color = BossBar.Color.NAMES.valueOr(config.getString("Color", "red"), BossBar.Color.RED);
+        overlay = BossBar.Overlay.NAMES.valueOr(config.getString("Overlay", "progress"), BossBar.Overlay.PROGRESS);
     }
 
+    /**
+     * Create a new Boss Bar for a player.
+     *
+     * @param player Player for whom a Boss Bar should be created.
+     */
     public void createBossBar(@NotNull Player player) {
         final Component text = formatBossBarText(player);
         final BossBar bossBar = BossBar.bossBar(text, progress, color, overlay);
@@ -52,23 +60,38 @@ public final class BossBarManager {
         player.showBossBar(bossBar);
     }
 
+    /**
+     * Remove a Boss Bar for a player.
+     *
+     * @param player Player for whom a Boss Bar should be removed.
+     */
     public void removeBossBar(@NotNull Player player) {
         final BossBar bossBar = bossBars.remove(player.getUniqueId());
         player.hideBossBar(bossBar);
     }
 
+    /**
+     * Update the Boss Bars for all players.
+     */
     public void updateBossBar() {
         for (final Player player : Bukkit.getOnlinePlayers()) {
             if (!bossBars.containsKey(player.getUniqueId())) {
                 createBossBar(player);
-            } else {
-                final Component text = formatBossBarText(player);
-                final BossBar bossBar = bossBars.get(player.getUniqueId());
-                bossBar.name(text);
+                continue;
             }
+
+            final Component text = formatBossBarText(player);
+            final BossBar bossBar = bossBars.get(player.getUniqueId());
+            bossBar.name(text);
         }
     }
 
+    /**
+     * Format the Boss Bar for a player, and replace Placeholders.
+     *
+     * @param player Player for whom the Boss Bar to format.
+     * @return Formated Boss Bar.
+     */
     private @NotNull Component formatBossBarText(@NotNull Player player) {
         String text = this.text;
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
